09f78e2e92ff5ed945e1882d9a00714b86f13754
Heba-Webdev23 <jtvonline@yahoo.com>
2022-01-20 20:17:00 +0300

    added new components

--- src/Card-holder.js
+++ src/Card-holder.js
@@ -1,14 +1,15 @@
 import React from "react"
-
-
 import './Card-holder.css';
 import CardInfo from "./card-info.js"
-
+import About from "./About"
+import Interests from "./Interests"
 
 export default function CardHolder() {
     return(
         <div className="card-holder">
            <CardInfo /> 
+           <About />
+           <Interests />
         </div>
     )
 }
