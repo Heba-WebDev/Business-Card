06d3f305edd341975af61353047feafb4b74370f
Heba-Webdev23 <jtvonline@yahoo.com>
2022-01-21 04:14:44 +0300

    fixed css isuess in about css

--- src/App.js
+++ src/App.js
@@ -11,18 +11,4 @@ function App() {
 }
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-       
-//         <p>
-//          Hi.
-//         </p>
-       
-//       </header>
-//     </div>
-//   );
-// }
-
 export default App;
